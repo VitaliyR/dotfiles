4e024b158519e001a02eadc5f4860111562d880b
Vitalii Rybachenko <vit@ribachenko.com>
2015-05-29 14:52:58 +0300

    Added nano config files and updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cd ~/.dotfiles
+
 # Bash
 echo '. ~/.dotfiles/terminal/init.sh' >> ~/.profile
+open ~/.dotfiles/terminal/TomorrowNight.itermcolors
 
 # Git
 ln -s git/.gitconfig ~
@@ -12,3 +15,14 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew tap homebrew/dupes
 bi $(<brew.txt)
 bup
+
+# Nano
+ln -s nano/.nanorc ~
+cd nano
+make install
+cd ../
+
+# Sublime
+cp sublime/Preferences.sublime-settings  ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+cp sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+cp sublime/Default\ (OSX).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ (OSX).sublime-keymap
--- pack.sh
+++ pack.sh
@@ -3,13 +3,11 @@
 # Pack everything is needed before sharing/comitting
 
 brew list > brew.txt
-cp ../.gitconfig ./
-cp ../.gitignore_global ./
-cp ../.nanorc ./
-cp ../.profile ./
 
-cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ./
-cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings ./
-cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ (OSX).sublime-keymap ./
+# Sublime
+
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings ./sublime
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings ./sublime
+cp ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ (OSX).sublime-keymap ./sublime
 
 echo 'Do not forget to setup your Sublime icon'
