8bc48d786ee31ce23b61bc0f81abc167dcf75701
Vitaliy Ribachenko <vit@ribachenko.com>
2016-04-26 15:28:36 +0300

    Add git completion

--- terminal/init.sh
+++ terminal/init.sh
@@ -1,6 +1,7 @@
 . ~/.dotfiles/terminal/highlight.sh
 . ~/.dotfiles/terminal/aliases.sh
 . ~/.dotfiles/terminal/help.sh
+. ~/.dotfiles/git/git-completion.sh
 
 export EDITOR=nano
 export PATH=$PATH:./node_modules/.bin
