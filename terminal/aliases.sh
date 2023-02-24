c758a92a8d2ce1c2f1dd87d9bf7d81548eb26931
Vitalii Rybachenko <vit@ribachenko.com>
2023-02-24 08:38:19 -0500

    Fix ls colors. Add node nvm version to prompt

--- terminal/aliases.sh
+++ terminal/aliases.sh
@@ -242,7 +242,7 @@ alias e='${(z)VISUAL:-${(z)EDITOR}}'
 
 # LS with color output and numberized chmod
 function ll {
- ls -lahG $1 | awk '{
+ ls -lahG --color=always $1 | awk '{
   k=0;
   for (i=0; i<=8; i++)
    k+= (( substr($1,i+2,1)~/[rwx]/ )*2^( 8-i ));
