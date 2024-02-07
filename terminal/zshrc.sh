bc312c12b2e3b03faf48de2180b9b0d1055d616f
Vitalii Rybachenko <vit@ribachenko.com>
2024-02-07 13:00:32 -0500

    Add vim to zsh

--- terminal/zshrc.sh
+++ terminal/zshrc.sh
@@ -22,3 +22,5 @@ export PATH="/opt/homebrew/bin:$curr/terminal/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source "$curr/terminal/vim.sh"
