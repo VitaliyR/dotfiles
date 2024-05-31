989789da0f06b1fa89bcc4829ec66486b06dc9dc
Vitalii Rybachenko <vit@ribachenko.com>
2024-05-31 14:35:00 -0400

    Update

--- terminal/zshrc.sh
+++ terminal/zshrc.sh
@@ -24,3 +24,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 source "$curr/terminal/vim.sh"
+
+# pnpm
+export PNPM_HOME="/Users/salen/Library/pnpm"
+case ":$PATH:" in
+  *":$PNPM_HOME:"*) ;;
+  *) export PATH="$PNPM_HOME:$PATH" ;;
+esac
+# pnpm end
