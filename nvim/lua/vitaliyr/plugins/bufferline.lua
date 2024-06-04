a2053d6d09a579ac576ae7bcad32d2ede8626288
Vitalii Rybachenko <vit@ribachenko.com>
2024-06-04 16:20:10 -0400

    Fix buffer

--- nvim/lua/vitaliyr/plugins/bufferline.lua
+++ nvim/lua/vitaliyr/plugins/bufferline.lua
@@ -1,8 +1,10 @@
 return {
-  'akinsho/bufferline.nvim',
-  version = '*',
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    require('bufferline').setup {}
-  end,
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	opts = {
+		options = {
+			diagnostics = "nvim_lsp",
+		},
+	},
 }
