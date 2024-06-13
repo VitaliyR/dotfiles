faf2cc2fa1b252cb909d1996127f7ba67fafe1dd
Vitalii Rybachenko <vit@ribachenko.com>
2024-06-13 10:52:42 -0400

    Dont save nvim-tree buffer in persistence

--- nvim/lua/vitaliyr/plugins/persistence.lua
+++ nvim/lua/vitaliyr/plugins/persistence.lua
@@ -1,18 +1,39 @@
-local persistance_group = vim.api.nvim_create_augroup('persistence', { clear = true })
-vim.api.nvim_create_autocmd('VimEnter', {
-  group = persistance_group,
-  callback = function()
-    require('persistence').load()
-  end,
-  nested = true
+local persistance_group = vim.api.nvim_create_augroup("persistence", { clear = true })
+vim.api.nvim_create_autocmd("VimEnter", {
+	group = persistance_group,
+	callback = function()
+		require("persistence").load()
+	end,
+	nested = true,
 })
 
 return {
-  'folke/persistence.nvim',
-  event = 'BufReadPre',
-  opts = { options = vim.opt.sessionoptions:get() },
-  keys = {
-    { '<leader>qs', function() require('persistence').load() end, desc = 'Restore Session' },
-    { '<leader>qd', function() require('persistence').stop() end, desc = 'Don\'t Save Current Session' },
-  },
+	"folke/persistence.nvim",
+	event = "BufReadPre",
+	opts = {
+		options = vim.opt.sessionoptions:get(),
+		pre_save = function()
+			for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+				if vim.api.nvim_buf_get_name(buf):match("/NvimTree_1$") then
+					vim.api.nvim_buf_delete(buf, {})
+				end
+			end
+		end,
+	},
+	keys = {
+		{
+			"<leader>qs",
+			function()
+				require("persistence").load()
+			end,
+			desc = "Restore Session",
+		},
+		{
+			"<leader>qd",
+			function()
+				require("persistence").stop()
+			end,
+			desc = "Don't Save Current Session",
+		},
+	},
 }
