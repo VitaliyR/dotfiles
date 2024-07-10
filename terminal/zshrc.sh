d7a9978a51914d7ce24dd81c368e590578a1472d
Vitalii Rybachenko <vit@ribachenko.com>
2024-07-10 12:52:09 -0400

    Fix config

--- terminal/zshrc.sh
+++ terminal/zshrc.sh
@@ -17,7 +17,7 @@ prompt 'vitalii'
 path=(/usr/local/opt/ruby/bin $path) # changing .zshenv doesn't work
 export GPG_TTY=$(tty) # For git commit signing
 
-export PATH="/opt/homebrew/bin:$curr/terminal/bin:$PATH"
+export PATH="/opt/homebrew/bin:$curr/terminal/bin:/opt/homebrew/opt/ruby/bin:$PATH"
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
